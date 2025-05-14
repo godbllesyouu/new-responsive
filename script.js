91848aecb44fd361a6784438e869cc61d4de2d28
Aziz <azizsmoyan19@gmail.com>
2025-05-14 20:23:09 +0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let arev = document.getElementsByClassName(".arev")
-let body = document.getElementsByName("body")
+let currentIndex = 0;
+const slider = document.querySelector('.cards-5');
+const totalCards = document.querySelectorAll('.card-5').length;
 
+function slideCard(direction) {
+  currentIndex += direction;
 
+  if (currentIndex < 0) {
+    currentIndex = totalCards - 1;
+  } else if (currentIndex >= totalCards) {
+    currentIndex = 0;
+  }
+
+  slider.style.transform = translateX(-${currentIndex * 100}%);
+}   
